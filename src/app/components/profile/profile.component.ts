560c6770378507fd96ddcfe4ec83b82b3fc20fda
Mark Ramy <markramyy@gmail.com>
2024-12-14 17:48:20 +0200

    add profile component with user information display and styling; integrate sidebar

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from '../../shared/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
