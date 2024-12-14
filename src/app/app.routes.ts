93dd58d133b342979ed9edfe17ec54adf19e966b
Mark Ramy <markramyy@gmail.com>
2024-12-14 19:19:59 +0200

    add authentication guard to dashboard and profile routes; enable route protection

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,19 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from './guard/auth.guard';
+
 import { LoginComponent } from './components/auth/login/login.component';
 import { SignupComponent } from './components/auth/signup/signup.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
     { path: 'signup', component: SignupComponent },
-    // { path: 'dashboard', component: DashboardComponent },
-    // { path: 'profile', component: ProfileComponent },
-    // { path: 'recipes', component: RecipesComponent },
-    // { path: 'plans', component: PlansComponent },
-    // { path: 'users', component: UsersComponent },
-    // { path: '**', redirectTo: 'login' }, // Wildcard for 404 page
+    { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+    { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
+    // { path: 'recipes', component: RecipesComponent, canActivate: [AuthGuard] },
+    // { path: 'plans', component: PlansComponent, canActivate: [AuthGuard] },
+    // { path: 'users', component: UsersComponent, canActivate: [AuthGuard] },
+    { path: '**', redirectTo: 'login' },
 ];
