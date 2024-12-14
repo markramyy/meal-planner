a318cd22a42424682980e8aaee20c90a30b902fb
Mark Ramy <markramyy@gmail.com>
2024-12-14 17:48:10 +0200

    enhance sidebar component with user profile and navigation links; add responsive styles

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule,
+    CommonModule
+  ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
