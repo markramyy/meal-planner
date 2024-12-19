1b28b60e1705564f1f92666800c579e5b8160c9c
Mark Ramy <markramyy@gmail.com>
2024-12-19 03:16:47 +0200

    fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { AuthGuard } from './guard/auth.guard';
 
 import { LoginComponent } from './components/auth/login/login.component';
 import { SignupComponent } from './components/auth/signup/signup.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { RecipesComponent } from './components/recipes/recipes.component';
 import { AddRecipeComponent } from './components/add-recipe/add-recipe.component';
@@ -14,7 +13,6 @@ export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
     { path: 'signup', component: SignupComponent},
-    { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
     { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
     { path: 'recipes', component: RecipesComponent, canActivate: [AuthGuard] },
     { path: 'recipes/add', component: AddRecipeComponent, canActivate: [AuthGuard] },
